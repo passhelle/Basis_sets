353581449ca8a18269bf113827fdc37b2d898a25
Pavel Shelyganov <p.a.shelyganov@gmail.com>
2020-11-12 11:41:52 +0100

    blankline test added

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -8,7 +8,13 @@ class TestStringMethods(unittest.TestCase):
   def test_main(self):
       Parser.main("test1.gjf", "", "")
       results = filecmp.cmp('test1_mod.gjf', 'test1_expected.gjf')
-      self.assertTrue(results)
+      self.assertTrue(results, "test1 crashed")
+
+
+  def test_space(self):
+      Parser.main("testspace.gjf", "", "")
+      results = filecmp.cmp('testspace_mod.gjf', 'testspace_expected.gjf')
+      self.assertTrue(results, "testspace crashed")
 
 
 if __name__ == '__main__':
