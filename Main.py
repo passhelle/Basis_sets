091c5cd7b45595741fdb450d2dd81a63184874d0
Pavel Shelyganov <p.a.shelyganov@gmail.com>
2020-11-12 11:35:17 +0100

    test added

--- Main.py
+++ Main.py
@@ -1,62 +1,8 @@
-from Find_Elements import FindEl
-from Make_Link import make_link
-import requests
+import Parser
+
+name = input("type in filename")
+bas = input("type in basis set") # issue with default parameters
+soft = input("type in software name") # issue with default parameters
 
 
-def copy_file(file_in, file_out):
-    with open(file_in) as f:
-        with open(file_out, "w") as f1:
-            for line in f:
-                f1.write(line)
-
-
-def clean_file(filename):
-    with open(filename) as f:
-        clean_list = []
-        for line in f:
-            clean_list.append(line)
-    last_index = len(clean_list) - 1
-    for i in range(len(clean_list) - 1, 0, -1):
-        if clean_list[i] != "":
-            last_index = i
-            break
-    clean_list = clean_list[0::last_index]
-    with open(filename, "w") as f1:
-        for line in clean_list:
-            f1.write(line)
-
-
-def main():
-    name = input("type in filename")
-    bas = input("type in basis set") # issue with default parameters
-    soft = input("type in software name") # issue with default parameters
-
-    """
-    name = "test1.gjf"
-    bas = ""
-    soft = ""
-    """
-
-    if bas == "":
-        bas = "def2-tzvp"
-    if soft == "":
-        soft = "gaussian94"
-    element_list = FindEl(name)
-    link = make_link(element_list, bas, soft)
-    r = requests.get(link)
-    text = r.text
-    text_list = []
-    text_list = text.split("\n")
-    text_list = text_list[10::]
-
-    parts = name.split(".")
-    name_out = parts[0]  + "_mod." + parts[1]
-    copy_file(name, name_out)
-    #clean_file(name_out)
-
-    for_out = "\n".join(text_list)
-    with open(name_out, "a") as ouf:
-        ouf.write(for_out)
-
-
-main()
+Parser.main(name, bas, soft)
