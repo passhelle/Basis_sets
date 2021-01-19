12363a27f46b4e5a427e7fb86ff5ce3c98bd4772
Pavel Shelyganov <p.a.shelyganov@gmail.com>
2021-01-19 22:22:39 +0100

    system arguments have been added

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
 import Parser
+import sys
+name = sys.argv[1]
+bas = sys.argv[2]
+if len(sys.argv) == 4:
+    soft = sys.argv[3]
+else:
+    soft = "gaussian94"
 
-name = input("type in filename")
-bas = input("type in basis set") # issue with default parameters
-soft = input("type in software name") # issue with default parameters
+#name = input("type in filename")
+#bas = input("type in basis set") # issue with default parameters
+#soft = input("type in software name") # issue with default parameters
 
 
 Parser.main(name, bas, soft)
